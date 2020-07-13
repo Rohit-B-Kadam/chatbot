2acfb62eba6f4f6213db805c83e5bbb4e6369dc0
rohit-b-kadam <rohitkadam1407@gmail.com>
2020-07-13 10:24:58 +0530

    backup to run on colab

--- models/joint_bert/vectorizers/bert_vectorizer.py
+++ models/joint_bert/vectorizers/bert_vectorizer.py
@@ -5,42 +5,39 @@ import tensorflow_hub as hub
 import numpy as np
 
 
-
 class BERTVectorizer:
-    
+
     def __init__(self, is_bert, bert_model_hub_path):
         self.is_bert = is_bert
         self.bert_model_hub_path = bert_model_hub_path
         self.create_tokenizer_from_hub_module(is_bert=is_bert)
-    
-        
+
     def create_tokenizer_from_hub_module(self, is_bert):
         """Get the vocab file and casing info from the Hub module."""
         # bert_module =  hub.Module(self.bert_model_hub_path)
         module_layer = hub.KerasLayer(self.bert_model_hub_path,
-                              trainable=False)
-        
+                                      trainable=False)
+
         if is_bert:
-            from vectorizers.tokenization import FullTokenizer
+            from models.joint_bert.vectorizers.tokenization import FullTokenizer
             vocab_file = module_layer.resolved_object.vocab_file.asset_path.numpy()
             do_lower_case = module_layer.resolved_object.do_lower_case.numpy()
             self.tokenizer = FullTokenizer(vocab_file, do_lower_case)
         else:
             sp_model_file = module_layer.resolved_object.sp_model_file.asset_path.numpy()
-            
+
             # commented and used the below instead because of lower case problem
             # from vectorizers.tokenization import FullSentencePieceTokenizer
             # self.tokenizer = FullSentencePieceTokenizer(sp_model_file)
-            from vectorizers.albert_tokenization import FullTokenizer
-            self.tokenizer = FullTokenizer(vocab_file=sp_model_file, 
-                                        do_lower_case=True,
-                                        spm_model_file=sp_model_file)
-        
+            from models.joint_bert.vectorizers.albert_tokenization import FullTokenizer
+            self.tokenizer = FullTokenizer(vocab_file=sp_model_file,
+                                           do_lower_case=True,
+                                           spm_model_file=sp_model_file)
+
         del module_layer
-    
-    
+
     def tokenize(self, text: str):
-        words = text.split() # whitespace tokenizer
+        words = text.split()  # whitespace tokenizer
         tokens = []
         valid_positions = []
         for i, word in enumerate(words):
@@ -52,8 +49,7 @@ class BERTVectorizer:
                 else:
                     valid_positions.append(0)
         return tokens, valid_positions
-    
-    
+
     def transform(self, text_arr):
         input_ids = []
         input_mask = []
@@ -66,14 +62,13 @@ class BERTVectorizer:
             segment_ids.append(seg_ids)
             valid_positions.append(valid_pos)
 
-        sequence_lengths = np.array([len(i) for i in input_ids])            
+        sequence_lengths = np.array([len(i) for i in input_ids])
         input_ids = tf.keras.preprocessing.sequence.pad_sequences(input_ids, padding='post')
         input_mask = tf.keras.preprocessing.sequence.pad_sequences(input_mask, padding='post')
         segment_ids = tf.keras.preprocessing.sequence.pad_sequences(segment_ids, padding='post')
         valid_positions = tf.keras.preprocessing.sequence.pad_sequences(valid_positions, padding='post')
         return input_ids, input_mask, segment_ids, valid_positions, sequence_lengths
-    
-    
+
     def __vectorize(self, text: str):
         tokens, valid_positions = self.tokenize(text)
         # insert "[CLS]"
@@ -82,10 +77,10 @@ class BERTVectorizer:
         # insert "[SEP]"
         tokens.append('[SEP]')
         valid_positions.append(1)
-        
+
         segment_ids = [0] * len(tokens)
         input_ids = self.tokenizer.convert_tokens_to_ids(tokens)
 
         input_mask = [1] * len(input_ids)
-        
+
         return input_ids, input_mask, segment_ids, valid_positions
